f5aac819f31a45afc27bd227c61c44f08f3e257e
Queenie-abosede <estheradewumi829@gmail.comgit config --global user.email estheradewumi829@gmail.comroot@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.comclear root@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.com>
2022-07-01 18:17:16 -0700

    task2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * this program prints "Programming is positive, zero, or negative
+ *
+ * Return: always return 0
+ */
+
+
 
 /* betty style doc for function main goes there */
 int main(void)
