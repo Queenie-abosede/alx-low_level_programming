6ff3dada4373d860863c2ebcaa50139b87db1825
Queenie-abosede <estheradewumi829@gmail.comgit config --global user.email estheradewumi829@gmail.comroot@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.comclear root@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.com>
2022-07-01 18:51:14 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,54 +2,37 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
 /**
- * main - entry point
- *
- * this program prints "Programming is positive, zero, or negative
- *
- * Return: always return 0
- */
+*main -> assign a random number to the variable n each time*it is executed
+*and print the last digit of the number stored in the varia*ble n
+*Return: Always 0 (Success)
+*/
 
-
-
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	
-	int l;
+int n;
+int x;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	l = n % 10;
+x = n % 10;
+printf("Last digit of %d is %d ", n, x);
+if (x > 5)
+{
+printf("and is greater than 5");
+}
+if (x == 0)
+{
+printf("and is 0");
+}
+if (x < 6 && x != 0)
+{
+printf("and is less than 6 and not 0");
+}
 
-	if (l > 5)
+printf("\n");
 
-	{
+return (0);
+}
 
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-	}
-
-	else if (l == 0)
-
-	{
-
-		printf("Last digit of %d is %d and is 0\n", n, l);
-
-	}
-
-	else
-
-	{
-
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-	}
-
-	return (0);
-}	
