1cc2d71a170d9cfa865cbfc27d362704acb5e660
Queenie-abosede <estheradewumi829@gmail.comgit config --global user.email estheradewumi829@gmail.comroot@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.comclear root@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.com>
2022-07-06 18:41:14 -0700

    nest0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  Prints _putchar
+ *
+ * Description: Program to print _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	if (n < 0)
+	n = -n;
+	x = n % 10;
+	_putchar (x = '0');
+	return (x);
+}
