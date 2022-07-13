ec950794e5a97fbf33b1a5b4f09f4c58b81cd9a4
Queenie-abosede <estheradewumi829@gmail.comgit config --global user.email estheradewumi829@gmail.comroot@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.comclear root@c7a1ef64c913:/# git config --global user.email "estheradewumi829@gmail.com>
2022-07-12 17:20:47 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - sets n to 98
- * @n: input integer
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: pointer to int n
  * Return: Always 0.
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
